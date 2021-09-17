1d841beb47c24461d63c804e52c2fd6f20dd68d3
Benjamin Wright <bwright@trilogyed.com>
2021-09-17 09:16:07 -0500

    Updates

---
+++ precache-manifest.45adaf2cd996e2af50cc0bda539d275f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d542c1ffa4108dec022e710a135f91f7",
+    "url": "/test-portfolio/index.html"
+  },
+  {
+    "revision": "b2f6ec58ecaf5576ba88",
+    "url": "/test-portfolio/static/css/main.883912da.chunk.css"
+  },
+  {
+    "revision": "ec7dea27f2d267470dff",
+    "url": "/test-portfolio/static/js/2.102548e6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/test-portfolio/static/js/2.102548e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2f6ec58ecaf5576ba88",
+    "url": "/test-portfolio/static/js/main.70b92a97.chunk.js"
+  },
+  {
+    "revision": "b9ecbaed25f5eebef053",
+    "url": "/test-portfolio/static/js/runtime-main.2659ef1c.js"
+  },
+  {
+    "revision": "a19f2b67a0540f784bf091db8e36c82a",
+    "url": "/test-portfolio/static/media/calculator.a19f2b67.jpg"
+  },
+  {
+    "revision": "402a471b5aad190a33b649f1368f5de1",
+    "url": "/test-portfolio/static/media/cover-image.402a471b.jpg"
+  },
+  {
+    "revision": "653c2b3d29f66a3c6e957518f3bd16f0",
+    "url": "/test-portfolio/static/media/led-wall.653c2b3d.jpg"
+  },
+  {
+    "revision": "ed494730868bd902e0ee98f1df1877e2",
+    "url": "/test-portfolio/static/media/pastel-puzzels.ed494730.jpg"
+  },
+  {
+    "revision": "938f8fe0fe2e5880b3ef528c5188a096",
+    "url": "/test-portfolio/static/media/run-buddy.938f8fe0.jpg"
+  },
+  {
+    "revision": "12557c9b108cf5e5347f8d838014b31e",
+    "url": "/test-portfolio/static/media/surf-report.12557c9b.jpg"
+  }
+]);
